f9f272f76ae0138b260a544cc890dec297ce1947
abdelrahman-sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2023-02-03 04:02:16 +0200

    leaves-request-result

--- assets/js/employee-dashboard.js
+++ assets/js/employee-dashboard.js
@@ -1,6 +1,6 @@
 // add active class to nav link 
 $('a:contains("لوحة التحكم للموظفين")').addClass('active');
-
+var example1 = flatpickr('.holiday-date');
 
 // get holidays options
 $.getJSON("https://seifeldeen.pythonanywhere.com/hr/list-avuser-leave/",function(data)
@@ -20,8 +20,9 @@ $.getJSON("https://seifeldeen.pythonanywhere.com/hr/list-avuser-leave/",function
 			
 
 
+
 // get welocme card
-$.getJSON(`http://seifeldeen.pythonanywhere.com/hr/emp-data/25/`, function (data) 
+$.getJSON(`http://seifeldeen.pythonanywhere.com/hr/emp-data/30/`, function (data) 
 	{
 
 			
@@ -38,7 +39,7 @@ $.getJSON(`http://seifeldeen.pythonanywhere.com/hr/emp-data/25/`, function (data
 	
 	
 				
-	// console.log(d)
+	// console.log(d)2
 	// add holiday_request
 	$('#add_request_holiday').click(function () 
 	{
@@ -50,13 +51,14 @@ $.getJSON(`http://seifeldeen.pythonanywhere.com/hr/emp-data/25/`, function (data
 			type: 'POST',
 			data: 
 			{
-				the_leave: $('#hodidays_option  option:selected').val(),
+				leave_name: $('#hodidays_option  option:selected').val(),
 				number_of_days_requested: $("#num_of_holiday").val(),
+				started_at: $("#start_date_of_holiday").val(),
 				available_for_this_user:data.id,		
 				success : function () 
 				{
-					// location.reload();
-					console.log('done')
+					location.reload();
+					// console.log('done')
 				}
 				
 				
@@ -66,11 +68,67 @@ $.getJSON(`http://seifeldeen.pythonanywhere.com/hr/emp-data/25/`, function (data
 
 
 	});
+	// get holiday result
+	$.getJSON("https://seifeldeen.pythonanywhere.com/hr/list-leave-request-user/", function (data2) 
+	{
 	
+		for(i in data2)
+		{
+			$('.holiday_request_result').append(`
+		
+			
+			
+				<tr class="row-${data2[i].the_leave.id}">
+					<td>${data2[i].the_leave.leave_name}</td> 
+					<td>${data2[i].number_of_days_requested} ايام</td>
+					
+					
+					<td class="text-center">
+						<div class="dropdown action-label">
+							<a class="btn btn-white btn-sm btn-rounded " href="#" data-bs-toggle="dropdown" aria-expanded="false">
+							
+							<i class="fa fa-dot-circle-o "id="currant_state-${data2.id}"></i> 
+							</a>
+							
+							
+					</td>
+				</tr>
+							
+			
+		
+		
+		
+					`)
+		}
+		
+		
+			console.log(data2[i].the_leave.id);
+					if(data2.accepted== true)
+					{
+						document.getElementById("currant_state-"+ data2.id).innerHTML = " موافقه"; 
+						document.getElementById("currant_state-"+ data2.id).classList.add("text-success");
+						document.getElementById("cahnge-state-"+ data2.id).remove();
+					}
+					else if(data.accepted==false)
+					{
+						document.getElementById("currant_state-"+ data2.id).innerHTML = "رفض"; 
+						document.getElementById("currant_state-"+ data2.id).classList.add("text-danger");
+						document.getElementById("cahnge-state-"+ data2.id).remove();
+						
+					}
+					else
+					{
+						document.getElementById("currant_state-"+ data2.id).innerHTML = " معلقة "; 
+						document.getElementById("currant_state-"+ data2.id).classList.add("text-purple");
+					}
+	
+				
+	})
+	
+		
+	
+
+
+
 	
 });
-
-
-
-
-
